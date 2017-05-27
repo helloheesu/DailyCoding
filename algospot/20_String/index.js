d1b41788def7a1ee4b62e82e659a2286cba5869b
helloheesu <hi.heesu820@gmail.com>
2017-05-27 16:34:54 +0900

    change spec and implement

--- algospot/20_String/index.js
+++ algospot/20_String/index.js
@@ -1,3 +1,22 @@
 function naiveSearch(haystack, needle) {
-	return -1;
+	if (!needle.length) {
+		return [];
+	}
+
+	var result = [];
+	for (var start = 0; start <= haystack.length - needle.length; start++) {
+		var matched = true;
+
+		for (var until = 0; until < needle.length; until++) {
+			if (haystack[start+until] !== needle[until]) {
+				matched = false;
+				break;
+			}
+		}
+
+		if (matched) {
+			result.push(start);
+		}
+	}
+	return result;
 }
--- algospot/20_String/test.js
+++ algospot/20_String/test.js
@@ -1,11 +1,63 @@
 describe("KMP > ", function() {
 	var testSearchFunction = naiveSearch;
-	it("빈 문자열에서 찾으면 -1", function() {
-		// when
-		var result = testSearchFunction("", "abcd");
-		// then
-		expect(result).to.be.eql(-1);
+
+	describe("가능한 시작 위치 다 반환하기", function() {
+		it("Blue Whale에서 Blue 찾으면 [0]", function() {
+			// given
+			var haystack = "Blue Whale";
+			var needle = "Blue";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([0]);
+		});
+		it("Blue Whale에서 Whale 찾으면 5", function() {
+			// given
+			var haystack = "Blue Whale";
+			var needle = "Whale";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([5]);
+		});
+		it("Blue Whale Blue에서 Blue 찾으면 [0, 11]", function() {
+			// given
+			var haystack = "Blue Whale Blue";
+			var needle = "Blue";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([0, 11]);
+		});
 	});
-	it("빈 문자열을 찾으면 length-1");
-	it("없는 문자열 찾으면 length-1");
+
+	describe.skip("없는 문자열 찾으면 빈 배열([]) 반환", function() {
+		it("빈 문자열에서 찾아도 없는 것", function() {
+			// given
+			var haystack = "";
+			var needle = "abcd";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([]);
+		});
+		it("빈 문자열을 찾아도 없는 것", function() {
+			// given
+			var haystack = "";
+			var needle = "abcd";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([]);
+		});
+		it("특정 문자열에서 없는 특정 문자열 찾기", function(argument) {
+			// given
+			var haystack = "Blue Whale";
+			var needle = "Blute";
+			// when
+			var result = testSearchFunction(haystack, needle);
+			// then
+			expect(result).to.be.eql([]);
+		});
+	});
 });
