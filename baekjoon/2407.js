e2de29d770f849ceab382a72568c70f2ab240b4a
helloheesu <hi.heesu820@gmail.com>
2017-05-30 22:16:41 +0900

    Hello World

--- baekjoon/2407.js
+++ baekjoon/2407.js
@@ -1,13 +1,34 @@
-// Combination
-// 100P9 > Number.MAX_SAFE_INTEGER
-// 파스칼의 삼각형 점화식으로는 stackoverflow
+/* [PASS] Combination
+ * 파스칼의 삼각형 점화식으로는 stackoverflow
+ * 100P9 > Number.MAX_SAFE_INTEGER
+ * StringSum 으로 DEALING WITH BIG NUMBER
+ */
 
 var fs = require('fs');
 var inputLine = fs.readFileSync('/dev/stdin').toString().split(' ');
 var n = parseInt(inputLine[0]);
 var m = parseInt(inputLine[1]);
 
-console.log((function Solution(n, m) {
+console.log(Solution(n, m));
+
+function stringSum(a, b) {
+	a = String(a).split('').reverse();
+	b = String(b).split('').reverse();
+
+	var result = [], len = Math.max(a.length, b.length);
+	for (var i = 0, pv = 0; i < len; i++) {
+		var digit = parseInt(a[i] || 0) + parseInt(b[i] || 0) + pv;
+		result[i] = digit % 10;
+		pv = Number(digit >= 10);
+	}
+
+	if (pv) {
+		result.push(pv);
+	}
+	return result.reverse().join('');
+}
+
+function Solution(n, m) {
 	if (m > n/2) {
 		m = n-m;
 	}
@@ -35,11 +56,11 @@ console.log((function Solution(n, m) {
 			// console.log('for', i, j);
 			var excluded = getComb(i-1, j-1);
 			var included = getComb(i-1, j);
-			var value = excluded + included;
+			var value = stringSum(excluded, included);
 			
 			setComb(i, j, value);
 		}
 	}
 
 	return getComb(n, m);
-})(n, m))
+}
