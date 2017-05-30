ce8c9e4094c0b3895008f5209859839245d1493f
helloheesu <hi.heesu820@gmail.com>
2017-05-30 22:02:49 +0900

    min/max 이런 사소한거 틀리지 말자

--- baekjoon/2004.js
+++ baekjoon/2004.js
@@ -1,8 +1,9 @@
-/* 조합 0의 개수
+/* [PASS] 조합 0의 개수
  * 스포당함 : 2가 적은 경우도 있음
  * countPow : 뒤집어서 생각하기
    - mySolution: 주어진 숫자보다 작거나 같은 최대 5의 거듭제곱 구하기
    - better : 5나누며 구하기
+ * min/max 이런 사소한거 틀리지 말자
 **/
 
 function countPow(number, base) {
@@ -22,5 +23,5 @@ var m = input[1];
 var consider5 = countPow(n, 5) - countPow(m, 5) - countPow(n-m, 5);
 var consider2 = countPow(n, 2) - countPow(m, 2) - countPow(n-m, 2);
 
-console.log(Math.max(consider5, consider2));
+console.log(Math.min(consider5, consider2));
 
