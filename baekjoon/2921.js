dec82e5b1a3acd97bfe77503cddc62defe1f9cab
helloheesu <hi.heesu820@gmail.com>
2017-05-30 22:25:43 +0900

    님아 자료형여 제발

--- baekjoon/2921.js
+++ baekjoon/2921.js
@@ -1,10 +1,11 @@
-/* 도미노
+/* [PASS] 도미노
  * 점 1개짜리가 몇번 나오나 : n+2
    - 0부터 N까지의 상대편에 존재 (n+1) + 자기자신도 상대편으로서 한 번 존재 (1)
  * -> 1부터 N까지 더한 값 * 각 점마다 나오는 횟수
    - 더한 값 ( n*(n+1)/2 ) * 횟수 ( n+2 )
+ * 하지만 틀림. 자료형!!!
  */
 
 var fs = require('fs');
-var n = fs.readFileSync('/dev/stdin').toString();
+var n = ParseInt(fs.readFileSync('/dev/stdin').toString());
 console.log(n*(n+1)/2*(n+2));
