1cc9ffbf0ed213185d8d7b28188d3608fdf4f0db
helloheesu <hi.heesu820@gmail.com>
2017-06-30 20:46:52 +0900

    follow the rule

--- baekjoon/4948.js
+++ baekjoon/4948.js
@@ -1,18 +1,18 @@
 // n보다 '크고' 2n보다 작거나 같은 <- 크고 빼먹음
 // countPrimes(isPrime, n+1, 2*n) <- n에 parseInt 안해서 n이 1이면 n+1은 11 넘어감
+// pop 말고 0 으로 검사
 
 function getIsPrime(untilNum) {
 	var isPrime = new Array(untilNum+1);
-	isPrime[2] = true;
 
 	// nest
-	for (var curPrime = 2; curPrime <= untilNum; curPrime++) {
+	var rooted = Math.floor(Math.sqrt(untilNum));
+	for (var curPrime = 2; curPrime <= rooted; curPrime++) {
 		if (isPrime[curPrime] === false) {
 			continue;
 		}
 
-		isPrime[curPrime] = true;
-		for (var multiple = curPrime*2; multiple <= untilNum; multiple += curPrime) {
+		for (var multiple = curPrime*curPrime; multiple <= untilNum; multiple += curPrime) {
 			isPrime[multiple] = false;
 		}
 	}
@@ -22,18 +22,32 @@ function getIsPrime(untilNum) {
 
 function countPrimes(isPrime, start, end) {
 	return isPrime.slice(start, end+1).reduce(function(count, isPrime) {
-		return count + (isPrime? 1: 0);
-	}, 0);
+		return count - (isPrime===false);
+	}, end - start + 1);
 }
 
 (function() {
 	var input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-	input.pop();
 
 	var isPrime = getIsPrime(123456*2);
 
 	for (var i = 0; i < input.length; i++) {
 		var n = parseInt(input[i]);
+		if (n === 0) {
+			break;
+		}
 		console.log(countPrimes(isPrime, n+1, 2*n));
 	}
 })();
+
+// (function() {
+// 	var isPrime = getIsPrime(1000000);
+// 	// var primes = [];
+// 	for (var i = 2; i < isPrime.length; i++) {
+// 		if (isPrime[i] !== false) {
+// 			// primes.push(i);
+// 			console.log(i);
+// 		}
+// 	}
+// 	// console.log(primes);
+// })();
