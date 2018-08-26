80a51c71641c0fbc99406f66357892dc85df4e4f
helloheesu <hi.heesu820@gmail.com>
2018-08-26 11:26:13 +0900

    Iterative Disjoin-set

--- baekjoon/1717.js
+++ baekjoon/1717.js
@@ -4,7 +4,7 @@
     var inputNum = meta[1];
 
     var inputs = rawInputs.slice(1, 1+inputNum);
-	solve(inputs);
+    solve(inputs, inputNum, meta[0]);
 })();
 
 function splitAndConvert(stringOfNumbers, numOfValid) {
@@ -13,21 +13,22 @@ function splitAndConvert(stringOfNumbers, numOfValid) {
 	});
 }
 
-function solve(inputs) {
+function solve(inputs, inputNum, N) {
     var parent = [];
 
-    for (var inputIdx = 0, len = inputs.length; inputIdx < len; inputIdx++) {
+    for (var inputIdx = 0; inputIdx < inputNum; inputIdx++) {
         var input = splitAndConvert(inputs[inputIdx], 3);
 
         if (input[0] === 0) { // operator === UNION
             Union(input[1], input[2]);
         } else { // operator === FIND
-            // console.log(Find(input[1]), Find(input[2]));
             var isInSameGroup = (Find(input[1]) === Find(input[2]));
             console.log(isInSameGroup? "YES": "NO");
         }
+        // console.log(parent);
     }
 
+    /*
     function Find(x) {
         if (parent[x] === undefined) {
             parent[x] = x;
@@ -39,6 +40,26 @@ function solve(inputs) {
         var foundParent = Find(parent[x]);
         parent[x] = foundParent;
         return foundParent;
+    }
+    */
+
+    function Find(x) {
+        if (parent[x] === undefined) {
+            parent[x] = x;
+            return x;
+        }
+
+        var temporalParents = [];
+        while(x !== parent[x]) {
+            temporalParents.push(x);
+            x = parent[x];
+        }
+
+        for (var i = 0, len = temporalParents.length; i < len; i++) {
+            parent[temporalParents[i]] = x;
+        }
+
+        return x;
     }
 
     function Union(x, y) {
